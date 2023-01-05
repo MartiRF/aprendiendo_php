dbc6255075df16b05ad513636177a142be1036eb
Martin Reyes <martinreyesrf@gmail.com>
2023-01-05 11:50:29 -0600

    aprendiendo php

---
+++ comentarios.php
@@ -0,0 +1,13 @@
+<?php
+
+    //Mi primer comentario en php
+
+    #Otro comentario con #
+
+    /*
+        Multiples lineas como en javaScript
+        Aprendiendo php en enero 5 de 2023
+    */
+    echo "Hola mundo";
+
+?>
---
+++ tipos_datos.php
@@ -0,0 +1,16 @@
+<?php
+    //boolean 
+    echo TRUE;
+    echo FALSE;
+    
+    // integers
+    var_dump(-10);
+    echo 10;
+    echo -10;
+
+    // Double o float
+    echo 7.5
+
+    // String
+    // echo 'String':
+?>
